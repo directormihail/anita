3441f8ebc3d2a25659a12f1959d04ca5811e8843
directormihail <130229452+directormihail@users.noreply.github.com>
2026-06-26 15:33:31 +0200

    Revert "first test"
    This reverts commit 65d780c1ef5da500c19b7e08087699c43d62fac1.

--- tiktok-hui-streak/uninstall-windows-task.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [string]$TaskName = "TikTokHuiStreak"
-)
-
-$ErrorActionPreference = "Stop"
-
-Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false
-Write-Host "Removed scheduled task '$TaskName'."
